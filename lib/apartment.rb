072fc11297efd5cef171b7c28ed726cf3e90068e
Saryn Mooney <smooney15@my.whitworth.edu>
2020-07-14 09:17:34 -0600

    iteration 1 complete

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -6,7 +6,11 @@ class Apartment
     @monthly_rent = info[:monthly_rent]
     @bathrooms = info[:bathrooms]
     @bedrooms = info[:bedrooms]
-    @renter = nil
+    @renter = renter
+  end
+
+  def add_renter(renter)
+    @renter = renter
   end
 
 end
--- test/apartment_test.rb
+++ test/apartment_test.rb
@@ -10,6 +10,26 @@ class ApartmentTest < Minitest::Test
     assert_instance_of Apartment, unit1
   end
 
+  def test_it_has_attributes
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    assert_equal "A1", unit1.number
+    assert_equal 1200, unit1.monthly_rent
+    assert_equal 1, unit1.bathrooms
+    assert_equal 1, unit1.bedrooms
+  end
+
+  def test_it_starts_with_no_renter
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    assert_nil unit1.renter
+  end
+
+  def test_it_can_add_renters
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    renter1 = Renter.new("Jessie")
+    unit1.add_renter(renter1)
+    assert_equal renter1, unit1.renter 
+  end
+
 end
 
 
