0ba817a33c7405a2b4b40a4a407eb1632faa0ce6
Saryn Mooney <smooney15@my.whitworth.edu>
2020-07-14 09:41:34 -0600

    renters method added

--- lib/building.rb
+++ lib/building.rb
@@ -1,8 +1,17 @@
 class Building
-  attr_reader :units
+  attr_reader :units, :renters
 
   def initialize
     @units = []
+    @renters = []
+  end
+
+  def add_unit(unit)
+    @units << unit
+  end
+
+  def add_renter(renter)
+    @renters << renter.name
   end
 
 end
--- test/building_test.rb
+++ test/building_test.rb
@@ -13,7 +13,37 @@ class BuildingTest < Minitest::Test
 
   def test_it_starts_with_no_units
     building = Building.new
-    assert_equal [], building.units 
+    assert_equal [], building.units
+  end
+
+  def test_it_can_add_units
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+    building.add_unit(unit1)
+    building.add_unit(unit2)
+    assert_equal [unit1, unit2], building.units
+  end
+
+  def test_it_starts_with_no_renters
+    building = Building.new
+    assert_equal [], building.renters
+  end
+
+  def test_it_can_add_renters
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+    building.add_unit(unit1)
+    renter1 = Renter.new("Aurora")
+    unit1.add_renter(renter1)
+    building.add_renter(renter1)
+    assert_equal ["Aurora"], building.renters
+    building.add_unit(unit2)
+    renter2 = Renter.new("Tim")
+    unit2.add_renter(renter2)
+    building.add_renter(renter2)
+    assert_equal ["Aurora", "Tim"], building.renters 
   end
 
 end
@@ -23,50 +53,14 @@ end
 # ## Iteration 2
 #
 # Use TDD to create a Building class that responds to the following interaction pattern.
-#
-# ```ruby
-# pry(main)> require './lib/renter'
-# # => true
-#
-# pry(main)> require './lib/apartment'
-# # => true
-#
-# pry(main)> require './lib/building'
-# # => true
-#
-# # => #<Building:0x00007f83778c5a80...>
-#
-# pry(main)> building.units
-# # => []
-#
-# pry(main)> unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
-# # => #<Apartment:0x00007f8377209bb0...>
-#
-# pry(main)> unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
-# # => #<Apartment:0x00007f83779f0900...>
-#
-# pry(main)> building.add_unit(unit1)
-#
-# pry(main)> building.add_unit(unit2)
-#
-# pry(main)> building.units
-# # => [#<Apartment:0x00007f8377209bb0...>, #<Apartment:0x00007f83779f0900...>]
-#
+
 # pry(main)> building.renters
 # # => []
 #
-# pry(main)> renter1 = Renter.new("Aurora")
-# # => #<Renter:0x00007fa83bc37978...>
-#
-# pry(main)> unit1.add_renter(renter1)
 #
 # pry(main)> building.renters
 # # => ["Aurora"]
 #
-# pry(main)> renter2 = Renter.new("Tim")
-# # => #<Renter:0x00007fa83b9b0358...>
-#
-# pry(main)> unit2.add_renter(renter2)
 #
 # pry(main)> building.renters
 # # => ["Aurora", "Tim"]
